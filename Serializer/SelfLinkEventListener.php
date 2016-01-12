5b252a021efbe16c7b57d198b95d11f33a27fc5b
Martin Poirier Theoret <mpoiriert@gmail.com>
2016-01-11 22:34:20 -0500

    add self link information

---
+++ Serializer/SelfLinkEventListener.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Draw\DrawBundle\Serializer;
+
+use JMS\Serializer\EventDispatcher\ObjectEvent;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Doctrine\Common\Util\ClassUtils;
+
+class SelfLinkEventListener
+{
+    private $entitiesRoutes = array();
+
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    public function __construct(ContainerInterface $container, $entitiesRoutes = array())
+    {
+        $this->container = $container;
+        $this->entitiesRoutes = $entitiesRoutes;
+    }
+
+    public function onPostSerialize(ObjectEvent $objectEvent)
+    {
+        $visitor = $objectEvent->getVisitor();
+        $object = $objectEvent->getObject();
+        try {
+            $class = ClassUtils::getClass($objectEvent->getObject());
+            foreach ($this->entitiesRoutes as $entityClass => $routeName) {
+                switch (true) {
+                    case $class == $entityClass:
+                    case is_subclass_of($class, $entityClass);
+                        $visitor->addData(
+                            '_href',
+                            $this->container->get("router")->generate(
+                                $routeName,
+                                array('id' => $this->container->get("property_accessor")->getValue($object, 'id'))
+                            )
+                        );
+                        break;
+                }
+            }
+        } catch (\InvalidArgumentException $e) {
+
+        }
+    }
+}
