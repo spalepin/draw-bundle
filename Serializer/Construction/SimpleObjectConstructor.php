9f303a4975ef937fc4008fc39d2995764f397235
Martin Poirier Theoret <mpoiriert@gmail.com>
2016-03-10 11:07:31 -0500

    add simple object constructor that just do a new so constructor is call

---
+++ Serializer/Construction/SimpleObjectConstructor.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Draw\DrawBundle\Serializer\Construction;
+
+use JMS\Serializer\Construction\ObjectConstructorInterface;
+use JMS\Serializer\DeserializationContext;
+use JMS\Serializer\Metadata\ClassMetadata;
+use JMS\Serializer\VisitorInterface;
+
+
+class SimpleObjectConstructor implements ObjectConstructorInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function construct(
+        VisitorInterface $visitor,
+        ClassMetadata $metadata,
+        $data,
+        array $type,
+        DeserializationContext $context
+    ) {
+        $className = $metadata->name;
+
+        return new $className();
+    }
+}
